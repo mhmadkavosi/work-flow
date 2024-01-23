913d4ffb78205eda75f5889e953e55037bec45e3
mhmad-kavosi <mhmad.kavosi@gmail.com>
2024-01-23 11:28:23 +0330

    add create steps and workflow

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.get_workflows),
-    path('<int:pk>/', views.get_workflow)
+    path('<int:pk>/', views.get_workflow),
+    path("create/", views.create_workflow),
+    path("step/create", views.create_step)
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
+from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .models import WorkFlow
-from .serializer import WorkflowSerializer
+from .models import WorkFlow, Step
+from .serializer import WorkflowSerializer, StepSerializer
 
 
 @api_view(["GET"])
@@ -19,3 +19,38 @@ def get_workflow(request, pk):
     query_set = get_object_or_404(WorkFlow, pk=pk)
     serializer = WorkflowSerializer(query_set)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def create_workflow(request):
+    serializer = WorkflowSerializer(data=request.data)
+
+    if not serializer.is_valid():
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['POST'])
+def create_step(request):
+    workflow_id = request.data.get("workflow_id")
+    workflow = get_object_or_404(WorkFlow, id=workflow_id)
+
+    step_data = {'workflow': workflow_id, **request.data}
+
+    last_step = Step.objects.filter(
+        workflow=workflow).order_by('-step_number').first()
+
+    step_number = last_step.step_number + 1 if last_step else 0
+
+    step_data['step_number'] = step_number
+
+    step_serializer = StepSerializer(data=step_data)
+
+    if not step_serializer.is_valid():
+        return Response(step_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    step_serializer.save()
+    return Response(step_serializer.data,
+                    status=status.HTTP_201_CREATED)
