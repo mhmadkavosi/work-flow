4254e7afc7ef4f836789970f093b602f3e7f2a40
mhmad-kavosi <mhmad.kavosi@gmail.com>
2024-01-31 10:15:42 +0330

    dockerize project

--- work_flow/settings.py
+++ work_flow/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'work_flow.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'work_flow',
-        'USER': 'kavosi',
-        'PASSWORD': 'kavosi',
-        'HOST': '127.0.0.1',
+        'NAME': os.environ.get("POSTGRES_NAME"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': 'db',
         'PORT': '5432'
     }
 }
